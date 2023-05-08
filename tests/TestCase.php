56ad2fcb323e196d009065d413feee6b455619e2
mintellity <mintellity@users.noreply.github.com>
2023-05-08 13:56:16 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Mintellity\LaravelRecycleBin\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Mintellity\LaravelRecycleBin\LaravelRecycleBinServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
